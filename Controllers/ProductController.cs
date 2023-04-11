8b345a162706e837dec0bfb81fa5b330ff884896
13 <13@DESKTOP-F3IR27Q>
2023-04-11 12:46:41 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,154 @@
+﻿using Crudapplication.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Crudapplication.Models;
+
+namespace Crudapplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ProductDAL _productDAL=new ProductDAL();
+        public ActionResult Index()
+        {
+            var productList=_productDAL.GetAllProducts();
+            if(productList.Count == 0)
+            {
+                TempData["InfoMessage"] = "Currently product not avaliable in the database";
+            }
+            return View(productList);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var product = _productDAL.GetProductsByID(id).FirstOrDefault();
+                if (product == null)
+                {
+                    TempData["InfoMessage"] = "Product not available with id " + id.ToString();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            bool IsInserted = false;
+            try
+            {
+              if(ModelState.IsValid)
+                {
+                    IsInserted=_productDAL.InsertProduct(product);
+                    if(IsInserted)
+                    {
+                        TempData["SuccessMessage"] = "Product details save successfully..!";
+                    }
+                    else
+                    {
+                        TempData["ErroMessage"] = "Product details is already available";
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = _productDAL.GetProductsByID(id).FirstOrDefault();
+            if (product == null)
+            {
+                TempData["ErroMessage"]="Product not available with ID"+id.ToString();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost,ActionName("Edit")]
+        public ActionResult UpdateProduct(Product product)
+        {
+            try
+            { 
+                if(ModelState.IsValid)
+                {
+                    bool IsUpdated = _productDAL.UpdateProduct(product);
+                    if (IsUpdated)
+                    {
+                        TempData["SuccessMessage"] = "Product details updated successfully..!";
+                    }
+                    else
+                    {
+                        TempData["ErroMessage"] = "Product details is already available";
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product=_productDAL.GetProductsByID(id).FirstOrDefault();
+            if(product == null)
+            {
+                TempData["ErroMessage"] = "Currently product not available with ID" + id.ToString();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    bool IsUpdated = _productDAL.DeleteProduct(id);
+                    if (IsUpdated)
+                    {
+                        TempData["SuccessMessage"] = "Product details deleted successfully..!";
+                    }                  
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErroMessage"] = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ DAL/ProductDAL.cs
@@ -0,0 +1,126 @@
+﻿using Crudapplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using Microsoft.ApplicationBlocks.Data;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Crudapplication.DAL
+{
+
+    public class ProductDAL
+    {
+        string conString = ConfigurationManager.ConnectionStrings["connectionstring"].ToString();
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> productList = new List<Product>();
+            string sqlquery = @"  select ProductID,ProductName,Price,Qty,Remark from tbl_ProductMaster";
+            DataSet objDataSet = SqlHelper.ExecuteDataset(conString, CommandType.Text, sqlquery);
+            DataTable dataTable = objDataSet.Tables[0];
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                productList.Add(new Product
+                {
+                    ProductID = Convert.ToInt32(dr["ProductID"]),
+                    ProductName = dr["ProductName"].ToString(),
+                    Price = Convert.ToDecimal(dr["Price"]),
+                    Qty = Convert.ToInt32(dr["qty"]),
+                    Remark = dr["Remark"].ToString()
+                });
+            }
+            return productList;
+        }
+
+        public bool InsertProduct(Product product)
+        {
+            int id = 0;
+            string strSql = @"insert into tbl_ProductMaster(ProductName,Price,Qty,Remark) values(@ProductName,@Price,@Qty,@Remark)";
+            SqlParameter[] objparam = new SqlParameter[]
+            {
+                new SqlParameter("@ProductName",product.ProductName),
+                new SqlParameter("@Price",product.Price),
+                new SqlParameter("@Qty",product.Qty),
+                new SqlParameter("@Remark",product.Remark),
+            };
+            id = SqlHelper.ExecuteNonQuery(conString, CommandType.Text, strSql, objparam);
+            if (id > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<Product> GetProductsByID(int ProductID)
+        {
+            List<Product> productList = new List<Product>();
+            string sqlquery = @"  select ProductID,ProductName,Price,Qty,Remark from tbl_ProductMaster where ProductID=@ProductID";
+            SqlParameter[] objparam = new SqlParameter[]
+            {
+                new SqlParameter("@ProductID", ProductID)
+            };
+            DataSet objDataSet = SqlHelper.ExecuteDataset(conString, CommandType.Text, sqlquery, objparam);
+            DataTable dataTable = objDataSet.Tables[0];
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                productList.Add(new Product
+                {
+                    ProductID = Convert.ToInt32(dr["ProductID"]),
+                    ProductName = dr["ProductName"].ToString(),
+                    Price = Convert.ToDecimal(dr["Price"]),
+                    Qty = Convert.ToInt32(dr["qty"]),
+                    Remark = dr["Remark"].ToString()
+                });
+            }
+            return productList;
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            int id = 0;
+            string strSql = @"update tbl_ProductMaster set ProductName=@ProductName,Price=@Price,Qty=@Qty,Remark=@Remark where ProductID=@ProductID";
+            SqlParameter[] objparam = new SqlParameter[]
+             {
+                new SqlParameter("@ProductID",product.ProductID),
+                new SqlParameter("@ProductName",product.ProductName),
+                new SqlParameter("@Price",product.Price),
+                new SqlParameter("@Qty",product.Qty),
+                new SqlParameter("@Remark",product.Remark),
+             };
+            id = SqlHelper.ExecuteNonQuery(conString, CommandType.Text, strSql, objparam);
+            if (id > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteProduct(int productID)
+        {
+            int i = 0;
+            string strSql = @"delete from tbl_ProductMaster where ProductId=@ProductId";
+            SqlParameter[] objparam = new SqlParameter[] {
+              new SqlParameter("@ProductID",productID)
+            };
+            int id=SqlHelper.ExecuteNonQuery(conString,CommandType.Text, strSql, objparam);
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
